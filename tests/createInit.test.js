6e4eb64eee065f1b41cb268b63a23d144fb6a31a
Thying <152802890+Thying@users.noreply.github.com>
2026-06-26 00:12:47 +0300

    проверка

--- tests/createInit.test.js
+++ tests/createInit.test.js
@@ -60,7 +60,6 @@ describe('createInit', () => {
 
     // Сначала инициализируем
     await init()(dispatch, getState)
-    // Потом чистим
     await clean()(dispatch, getState)
 
     // Проверяем, что save вызван с пустыми данными
