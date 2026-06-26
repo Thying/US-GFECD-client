7f8f623410ac49a35644c0a33ab7a0f87e5cb139
Thying <152802890+Thying@users.noreply.github.com>
2026-06-26 18:52:50 +0300

    Убрал доступ к регистрации что не лезли куда не надо

--- src/index.js
+++ src/index.js
@@ -2,6 +2,3 @@
 export { createInit, createMethod, createSub } from './store/factories'
 export { createSocket, getSocket } from './store/socket'
 
-// Registry (внутреннее, для продвинутого использования)
-export * from './store/registry'
-
